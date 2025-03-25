81eb88b1eea3fe60a8d96d4b377e873cef8bc0e8
marshacb <cameron.marshall12@gmail.com>
2025-03-24 21:56:53 -0400

    feat: add balances pallet

---
+++ src/balances.rs
@@ -0,0 +1,69 @@
+use std::collections::BTreeMap;
+
+pub struct Pallet {
+	balances: BTreeMap<String, u128>,
+}
+
+impl Pallet {
+	pub fn new() -> Self {
+		Self { balances: BTreeMap::new() }
+	}
+
+	pub fn set_balance(&mut self, who: &String, amount: u128) {
+		self.balances.insert(who.clone(), amount);
+	}
+
+	pub fn balance(&self, who: &String) -> u128 {
+		*self.balances.get(who).unwrap_or(&0)
+	}
+
+	pub fn transfer(
+		&mut self,
+		caller: String,
+		to: String,
+		amount: u128,
+	) -> Result<(), &'static str> {
+		let caller_balance = self.balance(&caller);
+		let recipent_balance = self.balance(&to);
+
+		let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Insufficient funds")?;
+		let new_recipient_balance =
+			recipent_balance.checked_add(amount).ok_or("An overflow occurred.")?;
+
+		self.set_balance(&caller, new_caller_balance);
+		self.set_balance(&to, new_recipient_balance);
+
+		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::Pallet;
+
+	#[test]
+	fn init_balances() {
+		let mut balances = Pallet::new();
+
+		assert_eq!(balances.balance(&"alice".to_string()), 0);
+		balances.set_balance(&String::from("alice"), 100);
+
+		assert_eq!(balances.balance(&"alice".to_string()), 100);
+		assert_eq!(balances.balance(&"bob".to_string()), 0);
+	}
+
+	#[test]
+	fn transfer_balances() {
+		let mut balances = Pallet::new();
+
+		assert_eq!(
+			balances.transfer("alice".to_string(), "bob".to_string(), 100),
+			Err("Insufficient funds")
+		);
+		balances.set_balance(&"alice".to_string(), 100);
+		balances.transfer("alice".to_string(), "bob".to_string(), 50);
+
+		assert_eq!(balances.balance(&"alice".to_string()), 50);
+		assert_eq!(balances.balance(&"bob".to_string()), 50);
+	}
+}
